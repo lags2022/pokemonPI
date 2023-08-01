4f16946329161626a9330248d375862ac7eb6951
lags2022 <lguzman.58erb@outlook.com>
2023-08-01 18:20:06 -0500

    update

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
