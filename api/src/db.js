69844aa8e164d5d6ad7807ec5f8289b9b7870bef
lags2022 <lguzman.58erb@outlook.com>
2023-08-01 09:53:53 -0500

    update

--- api/src/db.js
+++ api/src/db.js
@@ -5,11 +5,14 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?sslmode=require`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    ssl: true,
+    dialect: "postgres",
+    dialectoptions: {
+      ssl: true,
+    },
   }
 );
 const basename = path.basename(__filename);
